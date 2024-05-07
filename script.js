71d1e1fcf36cfc9bb021a44f391d9c5bb2fb2340
adrian <ag.serrano24@gmail.com>
2024-05-07 16:52:46 -0700

    Refactor index.html and script.js Add style.css
    Rewrote index.html to create landing page and buttons
    Rewrote script.js to utilizie DOM methods instead of having everything in console
    Created style.css to make landing page look nicer (duh)

--- script.js
+++ script.js
@@ -11,76 +11,83 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let gameLog = document.querySelector(".log");
+
     if (playerSelection == "rock") {
         if (computerSelection == "scissors") {
-            console.log("You win this round. Rock beats Scissors");
-            return 1;
+            gameLog.textContent = "You win this round. Rock beats Scissors";
+            return;
         } else if (computerSelection == "paper") {
-            console.log("You lose this round. Paper beats Rock");
+            gameLog.textContent = "You lose this round. Paper beats Rock";
             return 0;
         } else if (computerSelection == "rock") {
-            console.log("This round is a tie.");
+            gameLog.textContent = "This round is a tie.";
             return;
         }
     }
     if (playerSelection == "paper") {
         if (computerSelection == "scissors") {
-            console.log("You lose this round. Scissors beats Paper");
+            gameLog.textContent = "You lose this round. Scissors beats Paper";
             return 0;
         } else if (computerSelection == "paper") {
-            console.log("This round is a tie.");
+            gameLog.textContent = "This round is a tie.";
             return;
         } else if (computerSelection == "rock") {
-            console.log("You win this round. Paper beats Rock");
+            gameLog.textContent = "You win this round. Paper beats Rock";
             return 1;
         }
     }
     if (playerSelection == "scissors") {
         if (computerSelection == "scissors") {
-            console.log("This round is a tie.");
+            gameLog.textContent = "This round is a tie.";
             return;
         } else if (computerSelection == "paper") {
-            console.log("You win this round. Scissors beats Paper");
+            gameLog.textContent = "You win this round. Scissors beats Paper";
             return 1;
         } else if (computerSelection == "rock") {
-            console.log("You lose this round. Rock beats Scissors");
+            gameLog.textContent = "You lose this round. Rock beats Scissors";
             return 0;
         }
     }
-
-    // if user does not input a valid selection reprompt and run function again recursively
-    console.log("You did not input a valid play. Please try again.");
-    playerSelection = prompt("Please choose either 'Rock', 'Paper', or 'Scissors'");
-    playerSelection = playerSelection.toLowerCase();
-    playRound(playerSelection, computerSelection);
 }
 
 function playGame() {
-    // initializing counters before loop
     let playerCounter = 0;
     let compCounter = 0;
-    // run the game for 5 rounds and then determine winner
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Please choose either 'Rock', 'Paper', or 'Scissors'");
-        playerSelection = playerSelection.toLowerCase();
+
+    let rockButton = document.querySelector(".rock");
+    let paperButton = document.querySelector(".paper");
+    let scissorsButton = document.querySelector(".scissors");
+
+    rockButton.addEventListener("click", () => {
+        let playerSelection = "rock";
         let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
-        if (result == 0) {
-            compCounter++;
-        }
-        if (result == 1) {
-            playerCounter++;
-        }
-    }
-    if (playerCounter > compCounter) {
-        console.log("You've won the game!");
-    }
-    if (compCounter > playerCounter) {
-        console.log("You've lost the game, better luck next time!");
-    }
-    if (playerCounter == compCounter) {
-        console.log("The game ends in a tie!");
-    }
+        playRound(playerSelection, computerSelection);
+
+        
+    });
+
+    paperButton.addEventListener("click", () => {
+        let playerSelection = "paper";
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    });
+
+    scissorsButton.addEventListener("click", () => {
+        let playerSelection = "scissors";
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    });
+
+    // if (playerCounter > compCounter) {
+    //     gameLog.textContent("You've won the game!");
+    // }
+    // if (compCounter > playerCounter) {
+    //     gameLog.textContent("You've lost the game, better luck next time!");
+    // }
+    // if (playerCounter == compCounter) {
+    //     gameLog.textContent("The game ends in a tie!");
+    // }
 }
 
 playGame();
